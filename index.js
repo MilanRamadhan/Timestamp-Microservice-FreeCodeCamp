b8f09bdfcea4889f3e830cbd13885f84ae0ae344
milan <milan23@mhs.usk.ac.id>
2025-05-22 14:56:11 +0700

    update vercel.json

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-// index.js
-// where your node app starts
+// api/index.js
 
-// init project
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + "/../views/index.html");
 });
 
-// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-// Timestamp API Endpoint
 app.get("/api/:date?", function (req, res) {
   let dateInput = req.params.date;
 
-  // Jika tidak ada parameter, gunakan tanggal saat ini
   if (!dateInput) {
     const now = new Date();
     return res.json({
@@ -36,7 +26,6 @@ app.get("/api/:date?", function (req, res) {
     });
   }
 
-  // Jika hanya angka dan bukan ISO, artinya UNIX timestamp (dalam ms)
   if (!isNaN(dateInput) && /^\d+$/.test(dateInput)) {
     dateInput = parseInt(dateInput);
   }
@@ -53,7 +42,5 @@ app.get("/api/:date?", function (req, res) {
   });
 });
 
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+// Export app agar bisa digunakan oleh Vercel
+module.exports = app;
